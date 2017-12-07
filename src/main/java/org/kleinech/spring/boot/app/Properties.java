b4f0882865a869aa5bb233e8b4820d4d29f923db
Christoph Kleine <kleinech.github@gmail.com>
2017-12-07 09:20:51 +0100

    Update app
    Signed-off-by: Christoph Kleine <kleinech.github@gmail.com>

--- src/main/java/org/kleinech/spring/boot/app/Properties.java
+++ src/main/java/org/kleinech/spring/boot/app/Properties.java
@@ -1,6 +1,8 @@
 package org.kleinech.spring.boot.app;
 
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
+import javax.validation.constraints.Size;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.validation.annotation.Validated;
@@ -10,6 +12,12 @@ import org.springframework.validation.annotation.Validated;
 public class Properties {
 
   @NotNull
+  @Size(
+    min = 1,
+    max = 10,
+    message = "The length of the name must be between {min} and {max}."
+  )
+  @Pattern(regexp = "\\w+")
   private String name;
 
   public String getName() {
